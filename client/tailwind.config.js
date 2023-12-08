d015eebcb7ec0ec8f7e334f221d82fe16465f99b
Amanowsky <adamtroszczynski@outlook.com>
2023-12-08 22:16:12 +0100

    tailwind colors and fonts variables added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,8 +6,17 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    colors: {},
-    fontFamily: {},
+    colors: {
+      'black': '#000000',
+      'white': "#ffffff",
+      'blue': '#0019FF',
+      'lightBlue': '#0019ff1a',
+      'pastelRed': '#FF6262'
+    },
+    fontFamily: {
+      'merri': ['Merriweather', 'serif'],
+      'playfair': ['"Playfair Display"', 'serif']
+    },
     screens: {},
   },
   plugins: [
