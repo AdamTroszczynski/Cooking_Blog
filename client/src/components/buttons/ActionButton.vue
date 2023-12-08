e6ba67f3ec19acd1d942e71f07c615d3d3a04a39
Amanowsky <adamtroszczynski@outlook.com>
2023-12-08 22:26:43 +0100

    ActionButton and LinkButton components added

---
+++ client/src/components/buttons/ActionButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="px-6 btn" :class="[setButtonColor, setButtonBorder]">
+    <span class="font-[merri] text-[15px] font-black text-white normal-case"
+      ><slot></slot>
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const prop = defineProps({
+  isBlue: {
+    type: Boolean,
+    default: false,
+  },
+  biggerBorderRadius: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+/**
+ * Return status button classes
+ * @returns {string} status button classes
+ */
+const setButtonColor = computed((): string =>
+  prop.isBlue ? "bg-[blue]" : "bg-[#000000]"
+);
+
+/**
+ * Return status button classes
+ * @returns {string} status button classes
+ */
+const setButtonBorder = computed((): string =>
+  prop.biggerBorderRadius ? "rounded-[12px]" : "rounded-[8px]"
+);
+</script>
---
+++ client/src/components/buttons/LinkButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-link :to="linkTarget">
+    <span class="link link-neutral text-black font-[playfair] text-[15px] font-normal no-underline">
+      <slot></slot>
+    </span>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  linkTarget: {
+    type: String,
+    required: true,
+  },
+});
+</script>
