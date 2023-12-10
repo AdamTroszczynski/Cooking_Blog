6534abb7e17ee2966acba85df439f4d6bc4213d0
Amanowsky <adamtroszczynski@outlook.com>
2023-12-10 22:57:07 +0100

    Dish category section added, all dishes icon, categoryBox layoat added

---
+++ client/src/components/layoat/CategoryBox.vue
@@ -0,0 +1,45 @@
+<template>
+    <button @click="emitEvent"  class="btn flex justify-center items-center border border-black rounded-[9px]" :class="setBackground">
+        <slot></slot>
+    </button>
+</template>
+
+<script setup lang="ts">
+
+import { computed } from "vue";
+
+const prop = defineProps({
+    isSelected: {
+        type: Boolean,
+        required: false,
+        default: false
+    },
+    name: {
+        type: String,
+        required: true
+    }
+})
+
+
+
+const emit = defineEmits<{
+  /** Emit event after click button */
+  (e: "clickAction", data: string): void;
+}>();
+
+
+/** Emit event after click action */
+const emitEvent = () => {
+    emit('clickAction', prop.name)};
+
+/**
+ * Return status button classes
+ * @returns {string} status button classes
+ */
+const setBackground = computed(():string => {
+    return prop.isSelected ? 'bg-black' : 'bg-white'
+})
+
+
+
+</script>
