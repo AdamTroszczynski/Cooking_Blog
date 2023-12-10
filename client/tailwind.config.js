e190e495f87c87c5fce4a0ef3b6a14298efc77de
Amanowsky <adamtroszczynski@outlook.com>
2023-12-10 01:00:19 +0100

    TailWind screens options added, ActionButton class changed, TheHeader and MenuIcon components added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -16,7 +16,13 @@ module.exports = {
       merri: ['Merriweather', 'serif'],
       playfair: ['"Playfair Display"', 'serif']
     },
-    screens: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1538px'
+    },
   },
   plugins: [
     require('daisyui'),
