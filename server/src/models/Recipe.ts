b57c0e031030dbc6465adb296ab6dafd18980e95
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-12-13 17:23:33 +0100

    Created models, mappers, added first services

---
+++ server/src/models/Recipe.ts
@@ -0,0 +1,32 @@
+import Step from '@/models/Step';
+import Ingredient from '@/models/Ingredient';
+
+export default class Recipe {
+  recipeId: number;
+  recipeName: string;
+  created: number;
+  difficultLevel: string;
+  dishType: string;
+  userId: number;
+  steps: Step[];
+  ingredients: Ingredient[];
+  recipeImage: string;
+  likesCount: number;
+
+  constructor(
+    recipeId: number, recipeName: string, created: number,
+    difficultLevel: string, dishType: string, userId: number,
+    steps: Step[], ingredients: Ingredient[], recipeImage: string, likesCount: number
+  ) {
+    this.recipeId = recipeId;
+    this.recipeName = recipeName;
+    this.created = created;
+    this.difficultLevel = difficultLevel;
+    this.dishType = dishType;
+    this.userId = userId;
+    this.steps = steps;
+    this.ingredients = ingredients;
+    this.recipeImage = recipeImage;
+    this.likesCount = likesCount;
+  }
+};
--- server/src/services/instruction.md
+++
@@ -1,2 +0,0 @@
-## Here you can define your services
-Define here services to operate on PostgreSQL database
