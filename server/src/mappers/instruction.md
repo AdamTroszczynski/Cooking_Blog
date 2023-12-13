c4e4f0df67fc7808c05cd91de823ea9fbefc411b
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-12-13 17:23:33 +0100

    Created models, mappers, added first services

--- server/src/mappers/instruction.md
+++
@@ -1,2 +0,0 @@
-## Here you can define your mappers to map data from database to your models
-Here we should use mapper if we would like to operate on our models in some cases
---
+++ server/src/models/Step.ts
@@ -0,0 +1,9 @@
+export default class Step {
+  stepNumber: number;
+  stepContent: string;
+
+  constructor(stepNumber: number, stepContent: string) {
+    this.stepNumber = stepNumber;
+    this.stepContent = stepContent;
+  }
+};
