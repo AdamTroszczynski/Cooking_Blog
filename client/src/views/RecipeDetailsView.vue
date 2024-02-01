41d832262752943cd678f6338dbdd911a4889260
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2024-02-01 23:27:07 +0100

    Added logic for single recipe fetching when user open recipe details view

---
+++ client/src/views/RecipeDetailsView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-black">Recipe details view for recipe with id: {{ recipe.recipeId }}</div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRecipesStore } from '@/stores/recipesStore';
+import Recipe from '@/models/Recipe';
+
+const recipesStore = useRecipesStore();
+
+/**
+ * Get current loaded recipe
+ * @returns {Recipe} Loaded current recipe
+ */
+const recipe = computed<Recipe>(() => recipesStore.singleRecipe!);
+</script>
