33dab3f5804e148a5669871076bb2a62f240aaf8
Amanowsky <adamtroszczynski@outlook.com>
2024-02-26 19:26:06 +0100

    Added @returns commend

--- client/src/services/recipesServices.ts
+++ client/src/services/recipesServices.ts
@@ -55,6 +55,7 @@ export const saveRecipe = async (recipeToSave: any, token: string): Promise<Reci
  * Remove recipe with id `recipeId`
  * @param {number} recipeId Recipe id
  * @param {string} token Token
+ * @returns {Promise<Recipe>} Recipe id
  */
 export const removeRecipe = async (recipeId: number, token: string): Promise<void> => {
   const response = await axios.delete(`${RECIPE_API_URL}/removeRecipe/${recipeId}`,
