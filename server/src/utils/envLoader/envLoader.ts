203c7e09277a28ddeedb14d8be649625a3bc0015
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2024-02-29 13:41:39 +0100

    Prepare .env file for backend action

--- server/src/utils/envLoader/envLoader.ts
+++ server/src/utils/envLoader/envLoader.ts
@@ -8,9 +8,6 @@ const TEST_ENV_PATH = `${APP_ROOT}/../.env.test.local`;
 
 /** Load env variables based on NODE_ENV value */
 export const loadEnv = (): void => {
-  if (process.env.NODE_ENV === 'test' && !existsSync(DEV_ENV_PATH)) return;
-  else if (process.env.NODE_ENV !== 'test' && !existsSync(TEST_ENV_PATH)) return;
-
   dotenv.config({
     override: true,
     path: process.env.NODE_ENV === 'test' ? TEST_ENV_PATH : DEV_ENV_PATH,
