e6540de60cf86a48b8a4c344fe9caa09fe6152fd
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2024-02-29 12:11:26 +0100

    Don't load variables from env files if env files don't exist

--- server/src/utils/envLoader/envLoader.ts
+++ server/src/utils/envLoader/envLoader.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import dotenv from 'dotenv';
+import { existsSync } from 'fs';
 
 const APP_ROOT = path.resolve(__dirname);
 const DEV_ENV_PATH = `${APP_ROOT}/../.env`;
@@ -7,6 +8,9 @@ const TEST_ENV_PATH = `${APP_ROOT}/../.env.test.local`;
 
 /** Load env variables based on NODE_ENV value */
 export const loadEnv = (): void => {
+  if (process.env.NODE_ENV === 'test' && !existsSync(DEV_ENV_PATH)) return;
+  else if (process.env.NODE_ENV !== 'test' && !existsSync(TEST_ENV_PATH)) return;
+
   dotenv.config({
     override: true,
     path: process.env.NODE_ENV === 'test' ? TEST_ENV_PATH : DEV_ENV_PATH,
