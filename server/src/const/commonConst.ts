13e6888acb25d45a3da3443aa60ced37c1463a76
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2024-03-16 19:12:44 +0100

    fixed problem with different project source dir path + fixed upload image test

--- server/src/const/commonConst.ts
+++ server/src/const/commonConst.ts
@@ -5,3 +5,4 @@ export const PREVIEW_CLIENT_URL = `${BASE_URL}:4173`;
 export const RECIPE_API_PATH = '/api/recipe';
 export const AUTH_API_PATH = '/api/auth';
 export const STATIC_API_PATH = '/static';
+export const BASE_PROJECT_PATH = process.cwd();
